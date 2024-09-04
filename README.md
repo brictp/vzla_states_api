e033bfc186be383bdd5204928e1129ac01288110
brictp <brictp.1@gmail.com>
2024-09-04 14:50:44 -0400

    cambios en readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # vzla_states_api
+
+1. Agrega en tu entorno una variable PORT con el numero del puerto que vayas a usar
+2. En el mismo entorno agrega otra variable con el enlace a tu base de datos mongodb
+3. Ejecuta el codigo
--- config/db.js
+++ config/db.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+const connectString = process.env.DB_STRING;
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/vzla_api");
-    console.log('connection successful');
+    await mongoose.connect(connectString, {
+      autoIndex: true,
+    });
+    console.log("connection successful");
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
+};
 
 export default connectDB;
